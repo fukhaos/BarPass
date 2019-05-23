6c46ee8c92679809a63d727a5ec60a56b4abec05
Bruno Lopes <bruno.guitarpro@gmail.com>
2019-05-23 15:19:07 -0300

    a lot of images added into assets

--- BarPass/AppDelegate.swift
+++ BarPass/AppDelegate.swift
@@ -9,6 +9,7 @@
 import UIKit
 import OneSignal
 import FacebookCore
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-//        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.enable = true
         
         let navController = UINavigationController()
         
