16eaa850211779ae85103d210bf442341d34bd28
Bruno Lopes <bruno.guitarpro@gmail.com>
2019-05-29 17:16:52 -0300

    responsive vibration added to didselect item on tabbar

---
+++ BarPass/Dashboard/DashTabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  DashTabBarController.swift
+//  BarPass
+//
+//  Created by Bruno Lopes de Mello on 29/05/19.
+//  Copyright © 2019 Bruno Lopes. All rights reserved.
+//
+
+import UIKit
+import AudioToolbox
+import Spring
+
+class DashTabBarController: UITabBarController, UITabBarControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.delegate = self
+    }
+    
+
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        UIImpactFeedbackGenerator(style: .medium).impactOccurred()
+    }
+    
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        
+    }
+}
