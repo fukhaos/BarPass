4de4771da2601ad9d9fd302dee3101d5073b00e4
Bruno Lopes <bruno.guitarpro@gmail.com>
2019-05-31 09:17:44 -0300

    some endpoints removed

--- BarPass/Utils/URLs.swift
+++ BarPass/Utils/URLs.swift
@@ -25,22 +25,6 @@ enum URLs {
         }
     }
     
-    //main
-    static let recover = ""
-    
-    //occurrences
-    static let getOccurrences = "\(apiEndPoint)/Alert/GetNear"
-    static let saveOccurrence = "\(apiEndPoint)/Alert/Save"
-    static let deleteOccurrence = "\(apiEndPoint)/Alert/Delete"
-    static let getOccurrencesByUser = "\(apiEndPoint)/Alert/GetByUser"
-    static let getHelpedOccurrences = "\(apiEndPoint)/HelpSolicitation/GetAllMyAsHelper"
-    
-    //pet
-    static let savePet = "\(apiEndPoint)/Pet/Save"
-    static let deletePet = "\(apiEndPoint)/Pet/Delete"
-    static let getPet = "\(apiEndPoint)/Pet/Details"
-    static let getAllPets = "\(apiEndPoint)/Pet/GetAllMy/1/100"
-    
     //profile
     static let signin = "\(apiEndPoint)/Profile/Token"
     static let getInfo = "\(apiEndPoint)/Profile/GetInfo"
@@ -49,19 +33,8 @@ enum URLs {
     static let deleteUser = "\(apiEndPoint)/Profile/Delete"
     static let updateUser = "\(apiEndPoint)/Profile/Update"
     
-    //Vehicle
-    static let saveVehicle = "\(apiEndPoint)/Vehicle/Save"
-    static let deleteVehicle = "\(apiEndPoint)/Vehicle/Delete"
-    static let getVehicle = "\(apiEndPoint)/Vehicle/Details"
-    static let getAllVehicles = "\(apiEndPoint)/Vehicle/GetAllMy/1/100"
-    static let getAllBrands = "\(apiEndPoint)/Vehicle/GetAllBrand/1/100"
-    static let getModelByBrand = "\(apiEndPoint)/Vehicle/GetModelByBrandId/1/100"
-    
     //rating
     static let saveRating = "\(apiEndPoint)/Rating/Save"
-    
-    //helpSolicitation
-    static let saveHelp = "\(apiEndPoint)/HelpSolicitation/Save"
     
     //Terms of use
     static let termsAndUse = "\(baseUrl)/content/termos-de-uso.html"
@@ -69,17 +42,11 @@ enum URLs {
     //Privacy
     static let privacyPolicy = "\(baseUrl)/content/private-policy.html"
     
-    //About Guardioes
+    //About BarPass
     static let aboutGuardians = "\(baseUrl)/content/texto-institucional.html"
     
     //Suport
     static let supportContact = "\(apiEndPoint)/Support/Contact"
-    
-    //Emergency Contact
-    static let saveContact = "\(apiEndPoint)/EmergencyContact/Save"
-    static let deleteContact = "\(apiEndPoint)/EmergencyContact/Delete"
-    static let getAllContacts = "\(apiEndPoint)/EmergencyContact/GetAccepted/1/100"
-    static let getAllMeAsContact = "\(apiEndPoint)/EmergencyContact/GetMeAsContact"
     
     //Notifications
     static let getNotifications = "\(apiEndPoint)/Notifications/GetAllMy/1/100"
@@ -87,9 +54,6 @@ enum URLs {
     static let refuseEmergencyContact = "\(apiEndPoint)/EmergencyContact/RejectPermission"
     static let setAllUnreadToRead = "\(apiEndPoint)/Notifications/SetVisualized"
     static let deleteNotification = "\(apiEndPoint)/Notifications/Delete"
-    
-    //Chat
-    static let getChatList = "\(apiEndPoint)/Message/GetByAlert/1/100"
     
     //OneSignal
     static let oneSignal = "\(apiEndPoint)/Profile/RegisterUnRegisterDeviceId"
