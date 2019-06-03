17d5b51efd69aff927328ed2239d324c7b18aa74
Bruno Lopes <bruno.guitarpro@gmail.com>
2019-06-03 14:17:16 -0300

    halft of home screen is set

---
+++ BarPass/Home/View/HomeTableViewController.swift
@@ -0,0 +1,40 @@
+//
+//  HomeTableViewController.swift
+//  BarPass
+//
+//  Created by Bruno Lopes on 03/06/19.
+//  Copyright © 2019 Bruno Lopes. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 1
+    }
+}
