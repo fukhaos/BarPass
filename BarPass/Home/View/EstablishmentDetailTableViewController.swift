8054b8f95d1432bea912b86825b757e58ec4e8f3
Bruno Lopes <bruno.guitarpro@gmail.com>
2019-06-04 17:56:22 -0300

    commit to merge

--- BarPass/Home/View/EstablishmentDetailTableViewController.swift
+++ BarPass/Home/View/EstablishmentDetailTableViewController.swift
@@ -25,9 +25,6 @@ class EstablishmentDetailTableViewController: UITableViewController {
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     @IBAction func back(_ sender: Any) {
